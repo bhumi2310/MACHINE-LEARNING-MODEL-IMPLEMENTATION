9af02564d5f286c72d9bb594d1024990588c9ba6
bhumi2310 <sonalvsv1010@gmail.com>
2025-06-15 14:18:37 +0530

    Update task4.py

--- task4.py
+++ task4.py
@@ -4,23 +4,23 @@ from sklearn.naive_bayes import MultinomialNB
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score
 
-# Load sample spam dataset
+
 url = "https://raw.githubusercontent.com/justmarkham/pycon-2016-tutorial/master/data/sms.tsv"
 df = pd.read_csv(url, sep="\t", names=["label", "message"])
 
-# Feature extraction
+
 vectorizer = CountVectorizer()
 X = vectorizer.fit_transform(df['message'])
 y = df['label']
 
-# Split dataset into training and testing
+
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
 
-# Train model
+
 model = MultinomialNB()
 model.fit(X_train, y_train)
 
-# Predict and evaluate
+
 y_pred = model.predict(X_test)
 print("Accuracy:", accuracy_score(y_test, y_pred))
  
